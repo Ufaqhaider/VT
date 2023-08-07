58c5c000e0adaeac1832252376ad52b33dd085a5
Krishna Sahu <55760144+krishnacodes1@users.noreply.github.com>
2023-08-07 12:53:59 +0530

    contact form updated

--- src/contact/Contact_Us.js
+++ src/contact/Contact_Us.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
+import axios from "axios";
 
 const Contact_Us = () => {
+  const [name, setName] = useState("");
+  const [website, setWebsite] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //  console.log(name, website, email,message);
+
+    const data = {
+      Name: name,
+      Website: website,
+      Email: email,
+      Message: message,
+    };
+    axios
+      .post("https://sheetdb.io/api/v1/m1dppvbg5qawz", data)
+      .then((response) => {
+        // console.log(response);
+        setName("");
+        setWebsite("");
+        setEmail("");
+        setMessage("");
+      });
+  };
+
   return (
     <div className=" contact contactContainer  sectionContainer container snipcss-KKvc5">
       <div className="respo">
@@ -9,16 +36,28 @@ const Contact_Us = () => {
         <div className="contactTypes">
           <div className="flexContainer">
             <div className="typeBox">
-              <i className="lni lni-envelope"></i>
-              <a className="email" href="krishna.s@velocitytech.in">
+              {/* <i className="lni lni-envelope"></i> */}
+              <img
+                className="lni lni-envelope"
+                src="./images/Email.png"
+                alt="Envelope Icon"
+              />
+
+              <a className="email" href="mailto:krishna.s@velocitytech.in">
                 krishna.s@velocitytech.in
               </a>
+
               <p>Email Us</p>
             </div>
           </div>
           <div className="flexContainer">
             <div className="typeBox ">
-              <i className="lni lni-calendar"></i>
+              {/* <i className="lni lni-calendar"></i> */}
+              <img
+                className="lni lni-envelope"
+                src="./images/calender.png"
+                alt="Envelope Icon"
+              />
               <a
                 href="https://calendly.com/arupmandal731/15min"
                 target="_blank"
@@ -37,6 +76,7 @@ const Contact_Us = () => {
           id="contactForm"
           action="https://formspree.io/f/meqnladz"
           method="POST"
+          onSubmit={handleSubmit}
         >
           <div className="input1">
             <label for="nameInput" className="srOnly"></label>
@@ -45,49 +85,57 @@ const Contact_Us = () => {
               id="nameInput"
               name="nameInput"
               className="nameInput"
-              required=""
+              required
               placeholder="Organization Name*"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
             />
             <label for="emailInput" className="srOnly"></label>
             <input
               type="url"
-              id="emailInput"
-              name="emailInput"
-              className="emailInput"
-              required=""
+              id="urlnput"
+              name="urlnput"
+              className="urlnput"
+              required
               placeholder="Organization Website*"
+              onChange={(e) => setWebsite(e.target.value)}
+              value={website}
             />
           </div>
           <div className="input2">
             <label for="subjectInput" className="srOnly"></label>
             <input
               type="email"
-              id="subjectInput"
-              name="subjectInput"
+              id="emailInput"
+              name="emailInput"
               className="subjectInput"
-              required=""
+              required
               placeholder="Organization Email*"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
             <label for="messageInput" className="srOnly"></label>
             <input
               className="messageInput"
               name="messageInput"
               id="messageInput"
-              required=""
+              required
               placeholder="Your Message*"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
             ></input>
           </div>
-          <button
-          type="submit"
-          className="formSend"
-          target="_blank"
-          form="contactForm"
-          rel="noopener noreferrer"
-        >
-          SEND
-        </button>
+        
         </form>
-      
+        <button
+            type="submit"
+            className="formSend"
+            target="_blank"
+            form="contactForm"
+            rel="noopener noreferrer"
+          >
+            SEND
+          </button>
       </div>
     </div>
   );
